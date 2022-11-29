64e1431a9b6b3b4689956f005544b97497a10819
galee1025 <104541695+galee1025@users.noreply.github.com>
2022-11-29 14:19:53 +0900

    Update Day01_사칙연산.js

--- codingtest/Day01_사칙연산.js
+++ codingtest/Day01_사칙연산.js
@@ -1,1 +1,22 @@
+// 두 수의 합
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+function solution(num1, num2) {
+    var answer = num1 + num2;
+    return answer;
+}
+
+
+// 두 수의 차
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+const solution = (num1, num2) => num1 - num2
+
+
+// 두 수의 곱
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+const solution = (num1, num2) => num1*num2
+
+
+// 몫 구하기
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+const solution = (num1, num2) => Math.floor(num1 / num2)
 
