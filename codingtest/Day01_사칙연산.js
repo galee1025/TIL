29f6d8a72c4a9792d2621bea13d1ead3f23132a8
galee1025 <104541695+galee1025@users.noreply.github.com>
2022-11-30 16:34:20 +0900

    Update Day01_사칙연산.js

--- codingtest/Day01_사칙연산.js
+++ codingtest/Day01_사칙연산.js
@@ -2,8 +2,7 @@
 // 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
 
 function solution(num1, num2) {
-    var answer = num1 + num2;
-    return answer;
+    return num1 + num2;
 }
 
 
