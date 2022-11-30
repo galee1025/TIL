415aea0b07c6671e6d41e12a97045f4702eaba4a
galee1025 <104541695+galee1025@users.noreply.github.com>
2022-11-30 16:35:56 +0900

    Update Day02_사칙연산_조건문_배열.js

--- codingtest/Day02_사칙연산_조건문_배열.js
+++ codingtest/Day02_사칙연산_조건문_배열.js
@@ -1,4 +1,5 @@
 // 두 수의 나눗셈
+// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
 
 const solution = (num1, num2) => {
     let answer = num1/num2*1000
@@ -7,6 +8,7 @@ const solution = (num1, num2) => {
 
 
 // 숫자 비교하기
+// 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
 // if문
 
 const solution = (num1, num2) => {
@@ -26,11 +28,14 @@ const solution = (num1, num2) => {
 
 
 // 분수의 덧셈
+// 첫 번째 분수의 분자와 분모를 뜻하는 denum1, num1, 두 번째 분수의 분자와 분모를 뜻하는 denum2, num2가 매개변수로 주어집니다. 
+// 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 // 유클리드 호제법? 도전중!
 
 
 
 // 배열 두 배 만들기
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
 // for of 사용
 
 function solution(numbers) {
@@ -47,3 +52,5 @@ function solution(numbers) {
 const solution = (numbers) => {
     return numbers.map(number => number*2)
 }
+
+
